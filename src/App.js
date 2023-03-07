845f770919624e0d2d0269a8da180964bb25d054
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-07 14:03:25 +0000

    Install react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+import Button from "react-bootstrap/Button"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Welcome to Project Potato</h1>
+      <Button variant="primary">Click for potatoes!</Button>
     </div>
   );
 }
