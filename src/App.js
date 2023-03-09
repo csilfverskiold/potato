bce4be962333444c8722b2be59b6df15f2fdff23
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-09 18:28:27 +0000

    Create CurrentUserContext

--- src/App.js
+++ src/App.js
@@ -5,34 +5,9 @@ import { Route, Switch } from "react-router-dom";
 import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
-import { createContext, useEffect, useState } from "react";
-import axios from "axios";
-
-// Comment
-export const CurrentUserContext = createContext();
-export const SetCurrentUserContext = createContext();
 
 function App() {
-  // Persists the state of the currently logged in user
-  const [currentUser, setCurrentUser] = useState(null);
-
-  // Makes a network request to check who the user is
-  const handleMount = async () => {
-    try {
-      const { data } = await axios.get("dj-rest-auth/user/");
-      setCurrentUser(data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    handleMount();
-  }, []);
-
   return (
-    <CurrentUserContext.Provider value={currentUser}>
-      <SetCurrentUserContext.Provider value={setCurrentUser}>
         <div className={styles.App}>
           <NavBar />
           <Container className={styles.Main}>
@@ -44,8 +19,6 @@ function App() {
             </Switch>
           </Container>
         </div>
-      </SetCurrentUserContext.Provider>
-    </CurrentUserContext.Provider>
   );
 }
 
---
+++ src/contexts/CurrentUserContext.js
@@ -0,0 +1,33 @@
+import { createContext, useEffect, useState } from "react";
+import axios from "axios";
+
+// Storing logic in current user context and making API call
+export const CurrentUserContext = createContext();
+export const SetCurrentUserContext = createContext();
+
+export const CurrentUserProvider = ({ children }) => {
+  // Persists the state of the currently logged in user
+  const [currentUser, setCurrentUser] = useState(null);
+
+  // Makes a network request to check who the user is
+  const handleMount = async () => {
+    try {
+      const { data } = await axios.get("dj-rest-auth/user/");
+      setCurrentUser(data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    handleMount();
+  }, []);
+
+  return (
+    <CurrentUserContext.Provider value={currentUser}>
+      <SetCurrentUserContext.Provider value={setCurrentUser}>
+        {children}
+      </SetCurrentUserContext.Provider>
+    </CurrentUserContext.Provider>
+  );
+};
