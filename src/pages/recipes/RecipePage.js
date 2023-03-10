c570312757f1ad3b0c52dad7272d93a4b77d55dc
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-10 12:27:31 +0000

    Add functionality to fetch a recipe

--- src/pages/recipes/RecipePage.js
+++ src/pages/recipes/RecipePage.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
 import appStyles from "../../App.module.css";
+import { useParams } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
 function RecipePage() {
-  // Add your logic here
+  const { id } = useParams();
+  const [recipe, setRecipe] = useState({ results: [] });
 
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const [{ data: recipe }] = await Promise.all([
+          axiosReq.get(`/recipes/${id}`),
+        ]);
+        setRecipe({ results: [recipe] });
+        console.log(recipe);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [id]);
 
   return (
     <Row className="h-100">
