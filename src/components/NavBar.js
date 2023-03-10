23d0a5eec448866ac6532ddcf2136f0dfc468c84
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-10 06:32:04 +0000

    Create hook to handle closing navbar dropdown menu

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -11,10 +11,13 @@ import {
 } from "../contexts/CurrentUserContext";
 import Avatar from "./Avatar";
 import axios from "axios";
+import useClickOutsideToggle from "../hooks/useClickOutsideToggle";
 
 const NavBar = () => {
   const currentUser = useCurrentUser();
   const setCurrentUser = useSetCurrentUser();
+
+  const { expanded, setExpanded, ref } = useClickOutsideToggle();
 
   const handleSignOut = async () => {
     try {
@@ -61,7 +64,12 @@ const NavBar = () => {
   );
 
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed="top">
+    <Navbar
+      expanded={expanded}
+      className={styles.NavBar}
+      expand="md"
+      fixed="top"
+    >
       <Container>
         <NavLink to="/">
           <Navbar.Brand>
@@ -69,7 +77,11 @@ const NavBar = () => {
           </Navbar.Brand>
         </NavLink>
         {currentUser && addRecipeIcon}
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Toggle
+          ref={ref}
+          onClick={() => setExpanded(!expanded)}
+          aria-controls="basic-navbar-nav"
+        />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto text-right">
             {currentUser ? loggedInIcons : loggedOutIcons}
