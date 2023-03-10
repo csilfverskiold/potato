44598ffdd430c616098475b066b70a8bc39356fa
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-10 12:16:42 +0000

    Add basic structure for RecipePage

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import SignInForm from "./pages/auth/SignInForm";
 import RecipeCreateForm from "./pages/recipes/RecipeCreateForm";
+import RecipePage from "./pages/recipes/RecipePage";
 
 function App() {
   return (
@@ -17,6 +18,7 @@ function App() {
               <Route exact path="/signin" render={() => <SignInForm />} />
               <Route exact path="/signup" render={() => <SignUpForm />} />
               <Route exact path="/recipes/create" render={() => <RecipeCreateForm />} />
+              <Route exact path="/recipes/:id" render={() => <RecipePage />} />
               <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
