bbccdc002910d5315e01c44034ba0e8456601e1d
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-11 10:08:58 +0000

    Remove bootstrap dropdown menu template

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -5,52 +5,22 @@ import styles from "../styles/MoreDropdown.module.css";
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
 const ThreeDots = React.forwardRef(({ onClick }, ref) => (
-    <i
-      ref={ref}
-      onClick={(e) => {
-        e.preventDefault();
-        onClick(e);
-      }}
-    />
-  ));
-  
-  // forwardRef again here!
-  // Dropdown needs access to the DOM of the Menu to measure it
-  const CustomMenu = React.forwardRef(
-    ({ children, style, className, 'aria-labelledby': labeledBy }, ref) => {
-      const [value, setValue] = useState('');
-  
-      return (
-        <div
-          ref={ref}
-          style={style}
-          className={className}
-          aria-labelledby={labeledBy}
-        >
-          <FormControl
-            autoFocus
-            className="mx-3 my-2 w-auto"
-            placeholder="Type to filter..."
-            onChange={(e) => setValue(e.target.value)}
-            value={value}
-          />
-          <ul className="list-unstyled">
-            {React.Children.toArray(children).filter(
-              (child) =>
-                !value || child.props.children.toLowerCase().startsWith(value),
-            )}
-          </ul>
-        </div>
-      );
-    },
-  );
-  
-  render(
+  <i
+    ref={ref}
+    onClick={(e) => {
+      e.preventDefault();
+      onClick(e);
+    }}
+  />
+));
+
+export const MoreDropdown = ({ handleEdit, handleDelete }) => {
+  return (
     <Dropdown>
       <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
         Custom toggle
       </Dropdown.Toggle>
-  
+
       <Dropdown.Menu as={CustomMenu}>
         <Dropdown.Item eventKey="1">Red</Dropdown.Item>
         <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
@@ -59,5 +29,6 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
         </Dropdown.Item>
         <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
       </Dropdown.Menu>
-    </Dropdown>,
+    </Dropdown>
   );
+};
