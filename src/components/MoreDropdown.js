9f609ab384b04c0e2994e0ae3baa698e15ad68a7
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-11 10:05:25 +0000

    Add dropdown icon

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -4,18 +4,14 @@ import styles from "../styles/MoreDropdown.module.css";
 
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
-const CustomToggle = React.forwardRef(({ children, onClick }, ref) => (
-    <a
-      href=""
+const ThreeDots = React.forwardRef(({ onClick }, ref) => (
+    <i
       ref={ref}
       onClick={(e) => {
         e.preventDefault();
         onClick(e);
       }}
-    >
-      {children}
-      &#x25bc;
-    </a>
+    />
   ));
   
   // forwardRef again here!
