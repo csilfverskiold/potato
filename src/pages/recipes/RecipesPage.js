ee68eeb3ebee2274a2af3cca6119842891fd140d
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-11 08:01:10 +0000

    Add logic to display recipes list, including filters based on pathname

--- src/pages/recipes/RecipesPage.js
+++ src/pages/recipes/RecipesPage.js
@@ -1,20 +1,61 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 import Form from "react-bootstrap/Form";
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
 import Container from "react-bootstrap/Container";
 
+import Recipe from "./Recipe";
+import Asset from "../../components/Asset";
+
 import appStyles from "../../App.module.css";
 import styles from "../../styles/RecipesPage.module.css";
+import { useLocation } from "react-router";
+import { axiosReq } from "../../api/axiosDefaults";
 
-function RecipesPage() {
-  
+import NoResults from "../../assets/no-results.png";
+
+function RecipesPage({ message, filter = "" }) {
+  const [recipes, setRecipes] = useState({ results: [] });
+  const [hasLoaded, setHasLoaded] = useState(false);
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    const fetchRecipes = async () => {
+      try {
+        const { data } = await axiosReq.get(`/recipes/?${filter}`);
+        setRecipes(data);
+        setHasLoaded(true);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    setHasLoaded(false);
+    fetchRecipes();
+  }, [filter, pathname]);
+
   return (
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles mobile</p>
-        <p>List of recipes here</p>
+        {hasLoaded ? (
+          <>
+            {recipes.results.length ? (
+              recipes.results.map((recipe) => (
+                <Recipe key={recipe.id} {...recipe} setRecipes={setRecipes} />
+              ))
+            ) : (
+              <Container className={appStyles.Content}>
+                <Asset src={NoResults} message={message} />
+              </Container>
+            )}
+          </>
+        ) : (
+          <Container className={appStyles.Content}>
+            <Asset spinner />
+          </Container>
+        )}
       </Col>
       <Col md={4} className="d-none d-lg-block p-0 p-lg-2">
         <p>Popular profiles for desktop</p>
