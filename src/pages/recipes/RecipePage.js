ab2e6423e6e620f06e6468686c50bc84ceb7906d
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-11 13:52:51 +0000

    Add functionality to create a comment on a recipe

--- src/pages/recipes/RecipePage.js
+++ src/pages/recipes/RecipePage.js
@@ -8,10 +8,15 @@ import appStyles from "../../App.module.css";
 import { useParams } from "react-router";
 import { axiosReq } from "../../api/axiosDefaults";
 import Recipe from "./Recipe";
+import CommentCreateForm from "../comments/CommentCreateForm";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 function RecipePage() {
   const { id } = useParams();
   const [recipe, setRecipe] = useState({ results: [] });
+  const currentUser = useCurrentUser();
+  const profile_image = currentUser?.profile_image;
+  const [comments, setComments] = useState({ results: [] });
 
   useEffect(() => {
     const handleMount = async () => {
@@ -33,9 +38,19 @@ function RecipePage() {
     <Row className="h-100">
       <Col className="py-2 p-0 p-lg-2" lg={8}>
         <p>Popular profiles for mobile</p>
-        <Recipe {...recipe.results[0]} setRecipes={setRecipe} recipePage/>
+        <Recipe {...recipe.results[0]} setRecipes={setRecipe} recipePage />
         <Container className={appStyles.Content}>
-          Comments
+          {currentUser ? (
+            <CommentCreateForm
+              profile_id={currentUser.profile_id}
+              profileImage={profile_image}
+              recipe={id}
+              setRecipe={setRecipe}
+              setComments={setComments}
+            />
+          ) : comments.results.length ? (
+            "Comments"
+          ) : null}
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
