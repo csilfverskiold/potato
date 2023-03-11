fdd83150c827a28403d59dd7bde4d5aa72127268
csilfverskiold <christian.silfverskiold@gmail.com>
2023-03-11 15:21:10 +0000

    Add infinite scroll comp to recipe comments

--- src/pages/recipes/RecipePage.js
+++ src/pages/recipes/RecipePage.js
@@ -11,6 +11,9 @@ import Recipe from "./Recipe";
 import Comment from "../comments/Comment";
 import CommentCreateForm from "../comments/CommentCreateForm";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import InfiniteScroll from "react-infinite-scroll-component";
+import Asset from "../../components/Asset";
+import { fetchMoreData } from "../../utils/utils";
 
 function RecipePage() {
   const { id } = useParams();
@@ -54,14 +57,20 @@ function RecipePage() {
             "Comments"
           ) : null}
           {comments.results.length ? (
-            comments.results.map((comment) => (
-              <Comment
-                key={comment.id}
-                {...comment}
-                setRecipe={setRecipe}
-                setComments={setComments}
-              />
-            ))
+            <InfiniteScroll
+              children={comments.results.map((comment) => (
+                <Comment
+                  key={comment.id}
+                  {...comment}
+                  setRecipe={setRecipe}
+                  setComments={setComments}
+                />
+              ))}
+              dataLength={comments.results.length}
+              loader={<Asset spinner />}
+              hasMore={!!comments.next}
+              next={() => fetchMoreData(comments, setComments)}
+            />
           ) : currentUser ? (
             <span>No comments yet, be the first to comment!</span>
           ) : (
