c28f548a58f283b473f0f4ad7ae11447c0311568
csilfverskiold <christian.silfverskiold@gmail.com>
2023-04-17 09:56:56 +0000

    Add logic to display most saved recipes by title in PopularRecipes comp

--- src/pages/recipes/PopularRecipes.js
+++ src/pages/recipes/PopularRecipes.js
@@ -1,13 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
 import appStyles from "../../App.module.css";
-import { Container } from 'react-bootstrap';
+import Container from "react-bootstrap/Container";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import Asset from "../../components/Asset";
 
 const PopularRecipes = () => {
+  const [recipeData, setRecipeData] = useState({
+    // pageRecipe for later use
+    pageRecipe: { results: [] },
+    popularRecipes: { results: [] },
+  });
+  const { popularRecipes } = recipeData;
+  const currentUser = useCurrentUser();
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get("/recipes/?ordering=-saves_count");
+        setRecipeData((prevState) => ({
+          ...prevState,
+          popularRecipes: data,
+        }));
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    handleMount();
+  }, [currentUser]);
+
   return (
     <Container className={appStyles.Content}>
-        <p>Most saved recipes</p>
+      {popularRecipes.results.length ? (
+        <>
+          <p>Most saved recipes</p>
+          {popularRecipes.results.map((recipe) => (
+            <p key={recipe.id}>{recipe.title}</p>
+          ))}
+        </>
+      ) : (
+        <Asset spinner />
+      )}
     </Container>
-  )
-}
+  );
+};
 
-export default PopularRecipes
+export default PopularRecipes;
