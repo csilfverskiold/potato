806796b88d2a0a3cbf91a22cb3a36b2bdeeea78d
csilfverskiold <christian.silfverskiold@gmail.com>
2023-04-19 07:34:33 +0000

    Add logic to display updated follow button and profile page follow stats

--- src/contexts/ProfileDataContext.js
+++ src/contexts/ProfileDataContext.js
@@ -1,6 +1,7 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { axiosReq, axiosRes } from "../api/axiosDefaults";
 import { useCurrentUser } from "./CurrentUserContext";
+import { followHelper } from "../utils/utils";
 
 export const ProfileDataContext = createContext();
 export const SetProfileDataContext = createContext();
@@ -19,13 +20,21 @@ export const ProfileDataProvider = ({ children }) => {
 
   const handleFollow = async (clickedProfile) => {
     try {
-      const {data} = await axiosRes.post('/followers/', {
-        followed: clickedProfile.id
-      })
-    } catch(err) {
-      console.log(err)
+      const { data } = await axiosRes.post("/followers/", {
+        followed: clickedProfile.id,
+      });
+      setProfileData((prevState) => ({
+        ...prevState,
+        pageProfile: {
+          results: prevState.pageProfile.results.map((profile) =>
+            followHelper(profile, clickedProfile, data.id)
+          ),
+        },
+      }));
+    } catch (err) {
+      console.log(err);
     }
-  }
+  };
 
   useEffect(() => {
     const handleMount = async () => {
@@ -47,7 +56,7 @@ export const ProfileDataProvider = ({ children }) => {
 
   return (
     <ProfileDataContext.Provider value={profileData}>
-      <SetProfileDataContext.Provider value={{setProfileData, handleFollow}}>
+      <SetProfileDataContext.Provider value={{ setProfileData, handleFollow }}>
         {children}
       </SetProfileDataContext.Provider>
     </ProfileDataContext.Provider>
